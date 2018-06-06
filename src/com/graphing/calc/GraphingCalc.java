b4ecab54b57a6b01bb76b25ac6e786973d0c060a
seyon123 <r_seyon@live.ca>
2018-06-06 11:05:36 -0400

    Updated GUI

--- src/com/graphing/calc/GraphingCalc.java
+++ src/com/graphing/calc/GraphingCalc.java
@@ -1,8 +1,36 @@
 package com.graphing.calc;
 
-public class GraphingCalc {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class GraphingCalc extends JFrame {
+
+    private JPanel mainPanel;
+    private JButton submitButton;
+    private JComboBox  chooseFunction;
 
     public static void main (String [] args) {
-        
+        new GraphingCalc();
+    }
+
+    public GraphingCalc(){
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+
+        submitButton = new JButton();
+        submitButton.setPreferredSize(new Dimension(660, 30));
+        chooseFunction = new JComboBox();
+
+        mainPanel.add(submitButton);
+        mainPanel.add(chooseFunction);
+
+        setContentPane(mainPanel);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setTitle("Graphing Calculator");
+        setSize(800,800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
