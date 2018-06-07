e422009ccc9ed8b5240ad3e8c5f8e13a96aff4ef
lyjacky11 <lyjacky@live.com>
2018-06-06 22:57:11 -0400

    Added Glossary

--- src/com/graphing/calc/Glossary.java
+++ src/com/graphing/calc/Glossary.java
@@ -1,10 +1,28 @@
 package com.graphing.calc;
 
-import javax.swing.*;
+import java.util.*;
 
-public class Glossary extends JFrame {
+public class Glossary {
+
+    private ArrayList<String> glossary;
 
     public Glossary() {
+        glossary = new ArrayList<>();
+    }
 
+    private String defineWord(String word) {
+        int pos = glossary.indexOf(word) + 1;
+        return glossary.get(pos);
+    }
+
+    public boolean checkWord(String word) {
+        if (glossary.contains(word)) {
+            defineWord(word);
+        }
+        return false;
+    }
+
+    public String toString(String word) {
+        return defineWord(word);
     }
 }
