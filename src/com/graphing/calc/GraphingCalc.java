e2dd2c2ad9a5057e38edf1fea023c61244489b7e
seyon123 <r_seyon@live.ca>
2018-06-08 10:43:12 -0400

    Added rtn button

--- src/com/graphing/calc/GraphingCalc.java
+++ src/com/graphing/calc/GraphingCalc.java
@@ -9,7 +9,7 @@ import java.awt.event.*;
 public class GraphingCalc extends JFrame {
 
     private JPanel mainPanel;
-    private JButton submitButton, infoBtn;
+    private JButton submitButton, infoBtn, rtnBtn;
     private JComboBox  chooseFunction;
 
     public static void main (String [] args) {
@@ -27,8 +27,11 @@ public class GraphingCalc extends JFrame {
         infoBtn.setAlignmentX(RIGHT_ALIGNMENT);
 
         ImageIcon returnImg = new ImageIcon("rtnButton.png");
+        rtnBtn = new JButton(returnImg);
+        rtnBtn.addActionListener(new ButtonListener());
+        rtnBtn.setAlignmentX(LEFT_ALIGNMENT);
 
-        String[] functionList = {"-- None --", "Polynomial", "Exponential",  "Logarithmic",};
+        String[] functionList = {"-- None --", "Polynomial", "Exponential",  "Logarithmic"};
         chooseFunction = new JComboBox(functionList);
         chooseFunction.addActionListener(new ButtonListener());
         chooseFunction.setPreferredSize(new Dimension(200, 30));
@@ -61,6 +64,7 @@ public class GraphingCalc extends JFrame {
         setContentPane(new JLabel(new ImageIcon("background.jpg")));
         add(infoBtn);
         add(mainPanel);
+        add(rtnBtn);
         setLayout(new FlowLayout(FlowLayout.RIGHT));
         setTitle("Graphing Calculator");
         setSize(820,600);
