56f5e73b10411135a890b2cd7c531a969636b15e
seyon123 <r_seyon@live.ca>
2018-06-10 10:39:09 -0400

    Minor Changes - Screen tear glitch not fixed

--- src/com/graphing/calc/GraphingCalc.java
+++ src/com/graphing/calc/GraphingCalc.java
@@ -73,6 +73,7 @@ public class GraphingCalc extends JFrame {
         chooseFunction.addActionListener(new ButtonListener());
         chooseFunction.setPreferredSize(new Dimension(200, 30));
         chooseFunction.setMaximumSize(new Dimension(200, 30));
+        chooseFunction.setOpaque(false);
         chooseFunction.grabFocus();
 
         submitButton = new JButton("Enter");
