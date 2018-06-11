9beb5c808c0f393a267d757dc0f2f366c56d3bd6
seyon123 <r_seyon@live.ca>
2018-06-11 19:16:25 -0400

    Updated Polynomial class

--- src/com/graphing/calc/Polynomial.java
+++ src/com/graphing/calc/Polynomial.java
@@ -2,7 +2,52 @@ package com.graphing.calc;
 
 public class Polynomial extends Equations {
 
-    public Polynomial() {
+    private int coef1, coef2, coef3, coef4;
 
+    public Polynomial(int c1, int c2, int c3, int c4) {
+        coef1 = c1;
+        coef2 = c2;
+        coef3 = c3;
+        coef4 = c4;
     }
+
+    public Polynomial() {
+        coef1 = 0;
+        coef2 = 0;
+        coef3 = 0;
+        coef4 = 0;
+    }
+
+    public int getCoef1() {
+        return coef1;
+    }
+
+    public int getCoef2() {
+        return coef2;
+    }
+
+    public int getCoef3() {
+        return coef3;
+    }
+
+    public int getCoef4() {
+        return coef4;
+    }
+
+    public void setCoef1(int c1) {
+        coef1 = c1;
+    }
+
+    public void setCoef2(int c2) {
+        coef2 = c2;
+    }
+
+    public void setCoef3(int c3) {
+        coef3 = c3;
+    }
+
+    public void setCoef4(int c4) {
+        coef4 = c4;
+    }
+
 }
