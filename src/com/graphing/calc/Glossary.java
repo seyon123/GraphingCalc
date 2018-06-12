7e3a9f19dcd73c16b9f954e3f7eea9c9fc31153f
lyjacky11 <lyjacky@live.com>
2018-06-11 23:46:08 -0400

    Added binary search for glossary

--- src/com/graphing/calc/Glossary.java
+++ src/com/graphing/calc/Glossary.java
@@ -17,8 +17,34 @@ public class Glossary {
         inputFile.close();
     }
 
+    public static int binarySearch(ArrayList<String> glossary, String searchValue) {
+
+        int low = 0;
+        int high = glossary.size() - 1;
+        int mid = (low + high) / 2;
+
+        while (low <= high && !glossary.get(mid).equalsIgnoreCase(searchValue)) {
+
+            if (glossary.get(mid).compareTo(searchValue) < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+            mid = (low + high) / 2;
+
+            if (low > high) {
+                mid = -1;
+            }
+
+        }
+        return mid;
+
+    }
+
     public String defineWord(String word) {
-        int pos = glossary.indexOf(word) + 1;
+        //int pos = glossary.indexOf(word) + 1;
+        int pos = binarySearch(glossary, word) + 1;
         return glossary.get(pos);
     }
 
