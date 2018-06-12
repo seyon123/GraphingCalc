89a20305be7a5a0c745725418089d5f7f0f3a46e
lyjacky11 <lyjacky@live.com>
2018-06-12 19:49:12 -0400

    Canvas fixes

--- src/com/graphing/calc/GraphCanvas.java
+++ src/com/graphing/calc/GraphCanvas.java
@@ -11,6 +11,10 @@ public class GraphCanvas extends JPanel {
     private String parentFunction;
     private int coef1, coef2, coef3, coef4, coef5, base;
 
+    public GraphCanvas() {
+        repaint();
+    }
+
     public GraphCanvas(String function, String b) {
         parentFunction = function;
         base = Integer.parseInt(b);
