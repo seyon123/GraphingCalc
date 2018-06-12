421e782f609bd8ae8c8dc71c2a9fa2ea9bd0f1a9
lyjacky11 <lyjacky@live.com>
2018-06-12 16:52:08 -0400

    Test linear graph

--- src/com/graphing/calc/GraphCanvas.java
+++ src/com/graphing/calc/GraphCanvas.java
@@ -17,15 +17,11 @@ public class GraphCanvas extends JPanel {
         graph.drawGrid(g);
         g.setColor(Color.red);
 
-        int count = 0;
-            for(int i = 0; i < count; i++){
+            for(int i = 0; i < graph.getWidth(); i++){
                 if((graph.getyPoints(i) > 0 && graph.getyPoints(i) < graph.getHeight()) && (graph.gety2Points(i) > 0 && graph.gety2Points(i) < graph.getHeight())){
                     g.drawLine(i, graph.getyPoints(i), i - 1, graph.gety2Points(i));
                 }
             }
-            if(count < graph.getWidth() ){
-                count++;
-                repaint();
-        }
+
     }
 }
