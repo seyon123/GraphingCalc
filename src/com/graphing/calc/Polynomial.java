2fc879fe5b9b9c88bfac5aeecca3cdaded051e5a
seyon123 <r_seyon@live.ca>
2018-06-11 22:22:16 -0400

    Updated polynomials class and equations class

--- src/com/graphing/calc/Polynomial.java
+++ src/com/graphing/calc/Polynomial.java
@@ -2,7 +2,6 @@ package com.graphing.calc;
 
 public class Polynomial extends Equations {
 
-
     public Polynomial(int c1, int c2, int c3, int c4) {
         super(c1, c2, c3, c4, 0);
     }
@@ -19,4 +18,8 @@ public class Polynomial extends Equations {
         return "neither";
     }
 
+
+    public String toString() {
+        return super.toString();
+    }
 }
